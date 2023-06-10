dafd01af360a1aea5387f42c12e8dda415b5b389
HP <HP@LAPTOP-TJA3EU83>
2023-06-10 16:33:12 +0530

    This is 2nd time commit

--- GitHub_Project/src/dummyPackage/sampleClass1.java
+++ GitHub_Project/src/dummyPackage/sampleClass1.java
@@ -5,6 +5,7 @@ public class sampleClass1 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is sample class 1");
+		System.out.println("This is first time commit in GitHub");
 		
 	}
 
